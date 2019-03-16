4abf459d4f8f43fedb2052e9e90308f51f52a9a9
AlexLiu <alexsote@outlook.com>
2019-03-16 14:25:31 +0800

    update

--- src/modules/stats.ts
+++ src/modules/stats.ts
@@ -6,7 +6,7 @@ function InitStats() {
   stats.domElement.style.position = 'absolute'
   stats.domElement.style.top = '0'
   stats.domElement.style.left = '0'
-  stats.domElement.style.width = '130px'
+  //stats.domElement.style.width = '130px'
   let statsDiv: any = document.querySelector('#Stats-output')
   statsDiv.appendChild(stats.domElement)
   return stats
